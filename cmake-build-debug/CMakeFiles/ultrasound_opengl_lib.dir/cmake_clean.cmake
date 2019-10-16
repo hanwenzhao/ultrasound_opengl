d7fc8aac7c4ed19de4226e5bc33dd95d69b4a4a5
Hanwen Zhao <zhaolhw@gmail.com>
2019-10-16 00:56:02 -0400

    working graph

---
+++ cmake-build-debug/CMakeFiles/ultrasound_opengl_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ultrasound_opengl_lib.dir/main.cpp.o"
+  "libultrasound_opengl_lib.pdb"
+  "libultrasound_opengl_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ultrasound_opengl_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
